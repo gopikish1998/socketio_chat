cd0cb1d8eb97ee43db8b41659f937cab45f0825e
Gopi Kishore <kishorepenjendra@gmail.com>
2022-01-15 00:22:06 +0530

    api url updated

--- src/Settings.js
+++ src/Settings.js
@@ -1,7 +1,7 @@
 let getSettings = () => {
     return {
         // api : "http://localhost:4000",
-        api : "https://guvi-hackathon2-gopi.herokuapp.com",
+        api : "https://socketchatgopi.herokuapp.com",
     }
 }
 
