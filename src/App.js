42e40778c42157661ea803d67927c497932a7f94
Gopi Kishore <kishorepenjendra@gmail.com>
2022-01-15 00:15:55 +0530

    ui updated with authentication

--- src/App.js
+++ src/App.js
@@ -1,33 +1,58 @@
 import './App.css';
 import react,{useEffect, useState } from 'react';
 import io from "socket.io-client";
-
-
+import {BrowserRouter as Router, Route, Switch, Link} from 'react-router-dom'
+import Register from './Register'
+import Login from './Login'
+import Reset from './Reset'
+import Forgot from './Forgot'
+import Chat from './Chat';
+import { useHistory } from 'react-router-dom';
+import ChatProvider from './Context/ChatProvider';
+import HomePage from './HomePage';
+import { Typography } from '@mui/material';
+import CustomizedDialogs from './Chatcomponents/Profile';
 let socket;
 const CONNECTION_PORT = "https://socketchatgopi.herokuapp.com/";
 
 function App() {
+  const history = useHistory()
+  useEffect(() => {
+    if(/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)){
+  // true for mobile device
+      setMobile(true)
+      
+}else{
+  // false for not mobile device
+  setMobile(false)
+  const user = JSON.parse(window.localStorage.getItem('userinfo'))
+    if (user) {
+      // history.push('/chats')
+    }
+}  
+  }, [])
   const [loggedIn, setLoggedIn] = useState(false);
   const [room, setRoom] = useState("");
   const [userName, setUserName] = useState("");
-
+  const [isMobile, setMobile] = useState(false);
   // After Login
   const [message, setMessage] = useState("");
   const [messageList, setMessageList] = useState([]);
 
-  useEffect(() => {
-    socket = io(CONNECTION_PORT);
-  }, [CONNECTION_PORT]);
+  // useEffect(() => {
+  //   socket = io(CONNECTION_PORT);
+  // }, [CONNECTION_PORT]);
 
-  useEffect(() => {
-    socket.on("receive_message", (data) => {
-      setMessageList([...messageList, data]);
-    });
-  });
+  // useEffect(() => {
+  //   socket.on("receive_message", (data) => {
+  //     setMessageList([...messageList, data]);
+  //   });
+  // });
   const connectToRoom = () => {
     setLoggedIn(true);
     socket.emit("join_room", room);
   };
+
 
   const sendMessage = async () => {
     let messageContent = {
@@ -44,56 +69,19 @@ function App() {
   };
 
   return (
-    <div className="App">
-      {!loggedIn ? (
-        <div className="logIn">
-          <div className="inputs">
-            <input
-              type="text"
-              placeholder="Name..."
-              onChange={(e) => {
-                setUserName(e.target.value);
-              }}
-            />
-            <input
-              type="text"
-              placeholder="Room..."
-              onChange={(e) => {
-                setRoom(e.target.value);
-              }}
-            />
-          </div>
-          <button onClick={connectToRoom}>Enter Chat</button>
-        </div>
-      ) : (
-        <div className="chatContainer">
-          <div className="messages">
-            {messageList.map((val, key) => {
-              return (
-                <div
-                  className="messageContainer"
-                  id={val.author == userName ? "You" : "Other"}
-                >
-                  <div className="messageIndividual">
-                    {val.author}: {val.message}
-                  </div>
-                </div>
-              );
-            })}
-          </div>
-
-          <div className="messageInputs">
-            <input
-              type="text"
-              placeholder="Message..."
-              onChange={(e) => {
-                setMessage(e.target.value);
-              }}
-            />
-            <button onClick={sendMessage}>Send</button>
-          </div>
-        </div>
-      )}
+    <div>
+      
+      <Router>
+        <ChatProvider>
+        <Route path='/' component={HomePage} exact/>
+        <Route path='/login' component={Login} exact/>
+        <Route path='/register' component={Register} exact/>
+        <Route path='/reset' component={Reset} exact/>
+        <Route path='/forgot' component={Forgot} exact/>
+        <Route path='/chats' component={Chat} exact/>
+        <Route path='/test' component={CustomizedDialogs} exact/>
+        </ChatProvider> 
+      </Router>
     </div>
   );
 }
---
+++ src/Settings.js
@@ -0,0 +1,8 @@
+let getSettings = () => {
+    return {
+        // api : "http://localhost:4000",
+        api : "https://guvi-hackathon2-gopi.herokuapp.com",
+    }
+}
+
+export default getSettings()
