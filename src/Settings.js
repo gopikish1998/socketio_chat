c42730470910ff4810482a3d15a917b18f61d514
Penumaka Gopi Kishore <kishorepenjendra@gmail.com>
2024-03-21 22:06:36 +0530

    Update Settings.js

--- src/Settings.js
+++ src/Settings.js
@@ -1,7 +1,7 @@
 let getSettings = () => {
     return {
         // api : "http://localhost:4000",
-        api : "https://socketchatgopi.herokuapp.com",
+        api : "https://socket-chat-gopi-d47b2728f6a3.herokuapp.com",
     }
 }
 
